4c817be7c90308277ebfd9747543edf5f59f1e56
wei <admin@gaowei.in>
2020-09-17 07:28:40 +0800

    first

---
+++ S2.4/输入输出.java
@@ -0,0 +1,11 @@
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
+public class 输入输出 {
+    public static void main(String[] args) {
+        Scanner reader =new Scanner(System.in);
+        System.out.println(reader);
+    }
+
+    
+}
