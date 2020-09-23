dabb755dd2faf999ce5ed58d7f4c77eeb9a44a6b
wei <admin@gaowei.in>
2020-09-24 07:16:09 +0800

    2

--- S2.4/d918/unicode2int.java
+++ S2.4/d918/unicode2int.java
@@ -3,9 +3,9 @@ package d918;
 public class unicode2int {
     public static void main(String[] args) {
         /*
-        你的Unicode为 20320
-        我的Unicode为 25105
-        他的Unicode为 20182I
+        你的Unicode为 \u4f60
+        我的Unicode为 \u6211
+        他的Unicode为 \u4ed6
         */
         char a='你',b='我',c='他';
         System.out.println(a+"的Unicode为 \\u"+Integer.toHexString((int)a));
