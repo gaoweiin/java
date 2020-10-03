a3686240c0d04368f75a7c8c6c10e269f47c03e2
gaoweiin <admin@gaowei.in>
2020-10-03 14:32:15 +0800

    1003

---
+++ 课上/D1002/D1002.java
@@ -0,0 +1,21 @@
+package D1002;
+
+class Circle {
+    double rad = 0;
+    void setRadius(double r) {
+        rad = r;
+    }
+
+    void getRadius() {
+        System.out.println(rad);
+    }
+
+    double findArea() {
+        return(rad * rad * 3.14);
+    }
+}
+
+
+public class D1002 {
+    
+}
---
+++ 课上/D1002/stu.java
@@ -0,0 +1,23 @@
+package D1002;
+
+class Student {
+    int no;
+    String name;
+    int age;
+
+    void study() {
+        System.out.println("Study");
+    }
+
+    void sport() {
+        System.out.println("Sport");
+    }
+
+    void sleep() {
+        System.out.println("Sleep");
+    }
+    
+}
+public class stu {
+    
+}
