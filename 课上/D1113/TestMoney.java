290b9aef643df7a7e1ee1ce930916b432e5abe91
wei <admin@gaowei.in>
2020-11-13 10:48:01 +0800

    1113

--- 课上/D1113/TestMoney.java
+++ 课上/D1113/TestMoney.java
@@ -1,7 +1,6 @@
 package D1113;
 
 class BallanceNotEnoughException extends Exception {
-    
     public BallanceNotEnoughException(String message) {
         super(message);
     }
@@ -14,7 +13,7 @@ class Account {
         balance += money;
     }
 
-    void 支出out(double money) throws BallanceNotEnoughException{
+    void 支出out(double money) throws BallanceNotEnoughException {
         balance -= money;
         if (balance < 0) {
             throw new BallanceNotEnoughException("钱不够");
@@ -23,7 +22,6 @@ class Account {
 
 }
 
-
 public class TestMoney {
     public static void main(String[] args) {
         Account a = new Account();
@@ -31,9 +29,9 @@ public class TestMoney {
             a.收入in(500);
             a.支出out(1000);
         } catch (BallanceNotEnoughException e) {
-            //TODO: handle exception
+            // TODO: handle exception
             System.out.println(e.getMessage());
         }
-        
+
     }
 }
